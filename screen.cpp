7dd35ab035f5817a10dcf73258afb9be83e09a7b
Mikhail Solovyov <62037760+miksolovoff@users.noreply.github.com>
2020-10-18 18:46:11 +0300

    Update screen.cpp

--- screen.cpp
+++ screen.cpp
@@ -1,61 +1,34 @@
-#include "screen.h"
+#pragma once
 
-void Screen::setHwnd(const HWND& newHwnd) {
-	hwnd = newHwnd;
-}
+#include <Windows.h>
+#include <opencv2/highgui/highgui.hpp>
 
-Screen::Screen() {}
+using namespace cv;
 
-Screen::Screen(const HWND& newHwnd) {
-	// Параметры экрана
-	setHwnd(newHwnd);
-	hWDC = GetWindowDC(hwnd);
-	width = GetSystemMetrics(SM_CXSCREEN);
-	height = GetSystemMetrics(SM_CYSCREEN);
+class Screen {
+public:
+	Screen(const HWND& newHwnd);
+	Screen();
+	~Screen();
 
-	// Совместимый контекст в памяти
-	hScreen = CreateCompatibleDC(hWDC);
-	hBM = CreateCompatibleBitmap(hWDC, width, height); 
-	hBM_Temp = SelectObject(hScreen, hBM);
+	void setHwnd(const HWND& newHwnd);
 
-	// Описание рабочего массива бит совместимого контекста в памяти
-	BMI.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	BMI.bmiHeader.biWidth = width;
-	BMI.bmiHeader.biHeight = -height;
-	BMI.bmiHeader.biPlanes = 1;
-	BMI.bmiHeader.biBitCount = 24;
-	BMI.bmiHeader.biCompression = BI_RGB;
+	cv::Mat& get();
 
-	// Массив данных скриншота
-	const int colorSize = 3; // Размер пикселя
-	const int alignment = 4; // Выравнивание строки		 
-	int step = (int)ceil(width * colorSize / (double)alignment) * alignment; // Шаг строки
-	data = new char[step * height]; // - delete[] data;
+private:
+	HWND hwnd; // Р”РµСЃРєСЂРёРїС‚РѕСЂ РѕРєРЅР°
+	HDC hWDC; // РљРѕРЅС‚РµРєСЃС‚ СЌРєСЂР°РЅР°
+	HDC hScreen; // РЎРѕРІРјРµСЃС‚РёРјС‹Р№ СЃ СЌРєСЂР°РЅРѕРј РєРѕРЅС‚РµРєСЃС‚ РІ РїР°РјСЏС‚Рё
+	HBITMAP hBM; // Р Р°Р±РѕС‡РёР№ РјР°СЃСЃРёРІ Р±РёС‚ СЃРѕРІРјРµСЃС‚РёРјРѕРіРѕ РєРѕРЅС‚РµРєСЃС‚Р° РІ РїР°РјСЏС‚Рё
+	BITMAPINFO BMI; // РћРїРёСЃР°РЅРёРµ СЂР°Р±РѕС‡РµРіРѕ РјР°СЃСЃРёРІР° Р±РёС‚ СЃРѕРІРјРµСЃС‚РёРјРѕРіРѕ РєРѕРЅС‚РµРєСЃС‚Р° РІ РїР°РјСЏС‚Рё
 
-	// Скриншот
-	screen = new Mat(height, width, CV_8UC3, data, step);
-}
+	int width; // РЁРёСЂРёРЅР° СЌРєСЂР°РЅР° РІ РїРёРєСЃРµР»СЏС…
+	int height; // Р’С‹СЃРѕС‚Р° СЌРєСЂР°РЅР° РІ РїРёРєСЃРµР»СЏС…
 
-Screen::~Screen() {
-	SelectObject(hScreen, hBM_Temp);
-	DeleteObject(hBM);
-	DeleteDC(hScreen);
-	delete screen; screen = NULL;
-	delete[] data; data = NULL;
-}
+	char* data; // РњР°СЃСЃРёРІ РґР°РЅРЅС‹С… СЃРєСЂРёРЅС€РѕС‚Р°
+	cv::Mat* screen; // РЎРєСЂРёРЅС€РѕС‚
 
-Mat& Screen::get() {
-	BitBlt(hScreen, 0, 0, width, height, hWDC, 0, 0, SRCCOPY);
-	GetDIBits(hScreen, hBM, 0, height, data, &BMI, DIB_RGB_COLORS);
-	return *screen;
-}
+	HGDIOBJ hBM_Temp; // РњР°СЃСЃРёРІ Р±РёС‚ СЃРѕРІРјРµСЃС‚РёРјРѕРіРѕ РєРѕРЅС‚РµРєСЃС‚Р° РІ РїР°РјСЏС‚Рё СЃРѕР·РґР°РЅРЅС‹Р№ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ	
+};
 
-bool Equal(const Mat& lhs, const Mat& rhs) {
-	if ((lhs.rows != rhs.rows) || (lhs.cols != rhs.cols)) {
-		return false;
-	}
-
-	Scalar s = sum(lhs - rhs);
-
-	return (s[0] == 0) && (s[1] == 0) && (s[2] == 0);
-}
+bool Equal(const Mat& lhs, const Mat& rhs);
